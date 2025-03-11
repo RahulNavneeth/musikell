4d66160d40b9f13145fb1309acb3337b71f1c9d6
RahulNavneeth <rahulmnavneeth@gmail.com>
2025-03-11 17:03:21 +0530

    [feat/clean] : release, attack and other cleanups

--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ import Data.ByteString qualified as B
 import Data.ByteString.Builder as BB
 import Data.ByteString.Lazy (toStrict)
 
+import Data.List
 import System.Process (runCommand)
 import Text.Printf (printf)
 
@@ -13,8 +14,11 @@ type Hz = Float
 type RadS = Float
 type Pulse = Float
 
-semitone :: Float
-semitone = 1.05946
+semitone :: Hz
+semitone = 26.16
+
+pitchStandard :: Hz
+pitchStandard = 440.0
 
 sampleRate :: Hz
 sampleRate = 48000
@@ -22,17 +26,34 @@ sampleRate = 48000
 amplitude :: Float
 amplitude = 0.5
 
+bpm :: Float
+bpm = 120.0
+
+beatDuration :: Float
+beatDuration = 60.0 / bpm
+
 outputFilePath :: FilePath
 outputFilePath = "output.bin"
 
+-- SOURCE : https://pages.mtu.edu/~suits/NoteFreqCalcs.html
+f :: Float -> Hz
+f n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
+
+note :: Float -> Float -> [Pulse]
+note semitone beat = frequency (f semitone) (beat * beatDuration)
+
 frequency :: Hz -> Float -> [Pulse]
-frequency hz interval = (* amplitude) . sin . (* angular_velocity) <$> [0.0 .. sampleRate * interval]
+frequency hz interval = (* amplitude) <$> zipWith3 (\x y z -> x * y * z) release attack raw
   where
     angular_velocity :: RadS = frequency * 2 * 3.14 / sampleRate
     frequency :: Hz = hz -- A4 (pitch standard) (step)
+    attack :: [Pulse] = map (min 1.0) [0.0, 0.001 ..]
+    raw = sin . (* angular_velocity) <$> [0.0 .. sampleRate * interval]
+    release :: [Pulse] = reverse $ take (length raw) attack
 
 wave :: [Pulse]
-wave = concat [frequency 440.0 1, frequency 493.880416304 0.5, frequency 523.2465458574 1.0, frequency 587.3209588584 0.5, frequency 659.2416356024 1, frequency 698.4401432553 0.5, frequency 783.9679743508 1]
+-- wave = concat [frequency pitchStandard 1, frequency 493.880416304 0.5, frequency 523.2465458574 1.0, frequency 587.3209588584 0.5, frequency 659.2416356024 1, frequency 698.4401432553 0.5, frequency 783.9679743508 1]
+wave = concat [note i 1 | i <- [0, 2, 3, 5, 7, 8, 10, 12]]
 
 valueToByteString :: [Float] -> B.ByteString
 valueToByteString value = toStrict $ BB.toLazyByteString $ foldMap BB.floatLE value
@@ -46,4 +67,4 @@ play :: IO ()
 play = do
     save outputFilePath
     _ <- runCommand $ printf "ffplay -f f32le -ar 48000 %s" outputFilePath
-    return ()
+    putStrLn "Program executed succesfully!!!"
